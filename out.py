ef4c1024604a3cce957ac11febb61f838118ca67
soangrypanda <57560030+soangrypanda@users.noreply.github.com>
2021-02-16 21:42:28 +0200

    Create out.py

---
+++ out.py
@@ -0,0 +1,13 @@
+# THIS MODULE MAKES AN OUTPUT FILE
+
+def make_out_file(info, inp_name):
+    out_name = inp_name + ".out"
+    f = open(out_name, "w")
+    f.write(str(info["deliveries"])+"\n")
+    deliveries = info["piz_to_team"]
+    for team, pizzas in deliveries:
+        line = ""
+        line += str(team) + " "
+        line += " ".join([str(item) for item in pizzas])
+        f.write(line+"\n")
+    return
