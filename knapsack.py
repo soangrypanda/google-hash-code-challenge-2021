8b7d51c13a86142a61fe1dca532d709f3947e41c
soangrypanda <57560030+soangrypanda@users.noreply.github.com>
2021-02-17 21:13:40 +0200

    Now stored only two columns of dp per iteration, instead of the whole dp table

--- knapsack.py
+++ knapsack.py
@@ -15,42 +15,56 @@
 # In case current storage is less than a weight of a pizza, we do nothing.
 # What is great is that a weight of any pizza is always 1.
 
+from copy import deepcopy
+
+
 def dp_pizzas(problem, team_len):
+    print(f"Entering dp_pizzas with team len {team_len}...")
+
     piz_len = len(problem)
-    print(f"len of problem is {piz_len}")
-    print(f"team_len is {team_len}")
     if piz_len < team_len:
         sad_return = (set(), 0)
         return sad_return
+
     one_pizza_size = 1
 
-    dp = []
-    for i in range(0, piz_len+1):
-        line = []
-        for j in range(0, team_len+1):
-            line.append([set(), 0])
-        dp.append(line)
+    dp_empty = list()
+    for i in range(0, team_len + 1):
+        dp_empty.append([set(), 0])
+    dp_cur = deepcopy(dp_empty)
+    dp_prev = deepcopy(dp_empty)
 
     for i in range(piz_len - 1, -1, -1):
+        dp_prev.clear()
+        dp_prev = deepcopy(dp_cur)
+        dp_cur.clear()
+        dp_cur = deepcopy(dp_empty)
+
         for j in range(0, team_len + 1):
-            len1 = dp[i + 1][j][1]
+            len1 = dp_prev[j][1]
+
             if j >= one_pizza_size:
                 u_ing = set()
                 u_ing.update(problem[i][0])
-                for x in dp[i+1][j-one_pizza_size][0]:
+                for x in dp_prev[j-one_pizza_size][0]:
                     u_ing.update(problem[x][0])
+
                 len2 = len(u_ing)
+
                 if len1 > len2:
-                    dp[i][j][0] = dp[i+1][j][0].copy()
-                    dp[i][j][1] = len1
+                    dp_cur[j][0] = dp_prev[j][0].copy()
+                    dp_cur[j][1] = len1
                 else:
-                    dp[i][j][0] = dp[i+1][j-one_pizza_size][0].copy()
-                    dp[i][j][0].add(i)
-                    dp[i][j][1] = len2
+                    dp_cur[j][0] = dp_prev[j-one_pizza_size][0].copy()
+                    dp_cur[j][0].add(i)
+                    dp_cur[j][1] = len2
+
             else:
                 # technically, this branch is redundant in a case of this particular problem as
                 # all the cases falling into this branch are initially set to 0.
                 # and all these cases are 1) empty suffix and 2) 0 size knapsack.
-                dp[i][j][0] = dp[i + 1][j][0].copy()
-                dp[i][j][1] = len1
-    return dp[0][team_len]
+                dp_cur[j][0] = dp_prev[j][0].copy()
+                dp_cur[j][1] = len1
+
+    print("dp_pizzas is ready to return!")
+    return dp_cur[team_len]
