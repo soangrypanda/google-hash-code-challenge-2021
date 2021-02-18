afa122f48e8c2011a71a4f54e13e136cd050ba65
soangrypanda <57560030+soangrypanda@users.noreply.github.com>
2021-02-18 22:05:00 +0200

    added: handle_out_dir(), give_out_name(), give_inp_files()

--- out.py
+++ out.py
@@ -1,11 +1,29 @@
-# THIS MODULE MAKES AN OUTPUT FILE
+# THIS MODULE MAKES AN OUTPUT DIR (IF NECESSARY) AND OUTPUT FILES
 
-def make_out_file(info, inp_name):
+from pathlib import Path
+from os import mkdir, listdir
+from os.path import isfile, join
+
+
+def give_out_name(inp_name):
+    out_name = inp_name
+
+    if out_name[len(out_name) - 3:] == ".in":
+        out_name = out_name[:len(out_name) - 3] + ".out"
+    else:
+        out_name = out_name + ".out"
+
+    return out_name
+
+
+def make_out_file(info, inp_name, out_path):
     print("Entering make_out_file...")
 
-    out_name = inp_name + ".out"
+    out_name = out_path / give_out_name(inp_name)
+
     f = open(out_name, "w")
     f.write(str(info["deliveries"])+"\n")
+
     deliveries = info["piz_to_team"]
     for team, pizzas in deliveries:
         line = ""
@@ -15,3 +33,24 @@ def make_out_file(info, inp_name):
 
     print(f"File {out_name} created!")
     return
+
+
+def handle_out_dir(dir_path):
+    new_path = dir_path / "out"
+
+    if not new_path.is_dir():
+        print("No out directory! Creating...")
+        mkdir(str(new_path))
+
+    return new_path
+
+
+def give_inp_files(dir_path):
+    inp_path = dir_path / "inp"
+
+    if not inp_path.is_dir():
+        print("No folder with input files! Aborting program...")
+        exit(1)
+
+    inp_files = [f for f in listdir(inp_path) if isfile(join(inp_path, f))]
+    return inp_files, inp_path
